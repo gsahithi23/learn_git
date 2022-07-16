da8b03e2882cfb415e783cfd374592b221ac8f50
gsahithi23 <77588405+gsahithi23@users.noreply.github.com>
2022-07-16 11:30:22 -0700

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,28 @@
+let input = 'name'
+let alien = {
+    name : 'Sahithi',
+    technology : 'JS',
+    'work exp' : 2,
+    laptop : {
+        cpu : 'i7',
+        ram : 4,
+        brand1: 'Acer'
+    }
+}
+
+for(let key in alien){
+    console.log(key, alien[key])
+}
+
+
+// console.log(alien);
+// console.log(alien.name);
+// console.log(alien['technology']);
+// console.log(alien['work exp']); // cannot use . operator here since 'work exp' have spaces 
+// console.log(alien[input]); // cannot use . operator since input is not a key
+// //console.log(alien.laptop.brand.length);
+// console.log(alien.laptop.brand);//undefined
+// console.log(alien.laptop.brand?.length);
+
+// delete alien.laptop;
+// console.log(alien);
