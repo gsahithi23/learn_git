45b776a9a59774c779935a5d6312c451981333b2
gsahithi23 <77588405+gsahithi23@users.noreply.github.com>
2022-07-16 11:33:29 -0700

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,40 @@
+// let val = [2,3,5,6];
+// val.push(8);
+// console.log(val);
+// val.pop();
+// console.log(val);
+// val.shift();
+// //shift removes elements from start
+// console.log(val);
+// val.unshift(1);//Add values at the begining 
+// console.log(val);
+val2 = [2,3,4,5,6];
+console.log(val2.splice(2,1, 11, 15));
+console.log(val2)
+console.log(val2.push(8,10));
+
+
+
+
+
+// let values = [];
+// let value1 = [8,9,6];
+// values.push(5);
+// values.push(6);
+
+// //console.log(values);
+// //console.log(values.length);
+
+// //console.log(value1[1]);
+// //console.log(value1[3]);//no error we will get undefined if array is outbound
+
+// let names = ['sai', 'mena'];
+// names.push('Venu');
+// names[3] = 'Anu';
+
+// //console.log(names)
+
+// let data = ['Sahithi', 5, {tech: 'JS'}, function(){console.log("Hello")}]
+
+// console.log(data)
+// data[3]();
